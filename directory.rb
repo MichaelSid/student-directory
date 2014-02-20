ee8d7282f9cf5a6230b02148af3c33e5ebbb4037
Michael <michael.sidon@gmail.com>
2014-02-20 10:31:24 +0000

    Converted to hashes

--- directory.rb
+++ directory.rb
@@ -1,28 +1,28 @@
 #let's put all students into an array
 students = [
-"Mario Gintili",
-"Mikhail Dubov",
-"Karolis Noreika",
-"Michael Sidon",
-"Charles De Barros",
-"Ruslan Vikhor",
-"Toby Retallick",
-"Mark Mekhaiel",
-"Sarah Young",
-"Hannah Wight",
-"Khushkaran Singh",
-"Rick brunstedt",
-"Manjit Singh",
-"Alex Gaudiosi",
-"Ross Hepburn",
-"Natascia Marchese",
-"Tiffanie Chia",
-"Matthew Thomas",
-"Freddy McGroarty",
-"Tyler Rollins",
-"Richard Curteis",
-"Anna Yanova",
-"Andrew Cumine",
+{:name => "Mario Gintili", :cohort => :november},
+{:name => "Mikhail Dubov", :cohort => :november},
+{:name => "Karolis Noreika", :cohort => :november},
+{:name => "Michael Sidon", :cohort => :november},
+{:name => "Charles De Barros", :cohort => :novembr},
+{:name => "Ruslan Vikhor", :cohort => :november},
+{:name => "Toby Retallick", :cohort => :november},
+{:name => "Mark Mekhaiel", :cohort => :november},
+{:name => "Sarah Young", :cohort => :november},
+{:name => "Hannah Wight", :cohort => :november},
+{:name => "Khushkaran Singh", :cohort => :novembe},
+{:name => "Rick brunstedt", :cohort => :november},
+{:name => "Manjit Singh", :cohort => :november},
+{:name => "Alex Gaudiosi", :cohort => :november},
+{:name => "Ross Hepburn", :cohort => :november},
+{:name => "Natascia Marchese", :cohort => :novembr},
+{:name => "Tiffanie Chia", :cohort => :november},
+{:name => "Matthew Thomas", :cohort => :november},
+{:name => "Freddy McGroarty", :cohort => :novembe},
+{:name => "Tyler Rollins", :cohort => :november},
+{:name => "Richard Curteis", :cohort => :november},
+{:name => "Anna Yanova", :cohort => :november},
+{:name => "Andrew Cumine", :cohort => :november}
 ]
 
 def print_header
@@ -30,9 +30,9 @@ puts "The students of my cohort at Makers Academy"
 puts "-------------"
 end
 
-def print(names)
-names.each do |name|
-	puts name
+def print(students)
+students.each do |student|
+	puts "#{student[:name]} (#{student[:cohort]} cohort)"
 end
 end
 
