c9d949d5c160d58bb0ec6c15300a54520d5f7e29
Michael <michael.sidon@gmail.com>
2014-02-20 15:00:06 +0000

    First exercise index names

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
 #let's put all students into an array
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	print "Please enter the names of the students \n"
+	print "To finish, just hit return twice \n"
 	#create an empty array
 	students = []
 	#get the first name
@@ -10,7 +10,7 @@ def input_students
 	while !name.empty? do
 		#add the student hash to the array
 		students << {:name => name, :cohort => :november}
-		puts "Now we have #{students.length} students."
+		print "Now we have #{students.length} students. \n"
 		name = gets.chomp
 	end
 	#return the array of students
@@ -18,24 +18,24 @@ def input_students
 end
 
 def print_header
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
+	print "The students of my cohort at Makers Academy \n"
+	print "------------- \n"
 end
 
-def print(students)
-students.each do |student|
-	puts "#{student[:name]} (#{student[:cohort]} cohort)"
-end
+def print_students(students)
+	students.each_with_index do |student, index|
+	  	print "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort) \n"
+	end
 end
 
 def print_footer(names)
-puts "Overall, we have #{names.length} great students"
+	print "Overall, we have #{names.length} great students \n"
 end
 
 #nothing happens until we call the methods
 students = input_students
 print_header
-print(students)
+print_students(students)
 print_footer(students)
 
 	
